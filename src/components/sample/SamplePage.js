726b2b925e05a107354fc0044ef8ecf4846484c3
anhvodhvn <anh.voduy@harveynash.vn>
2018-04-16 17:01:38 +0700

    update control: react-select

--- src/components/sample/SamplePage.js
+++ src/components/sample/SamplePage.js
@@ -1,16 +1,25 @@
 import React, {PropTypes} from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+
 import DemoProgressBar from './DemoProgressBar';
 import DemoDropdownMenu from './DemoDropdownMenu';
 import NavMultiLevel from '../common/NavMultiLevel';
 
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+const list = [
+    { value: 'one', label: 'One' },
+    { value: 'two', label: 'Two' },
+    { value: 'three', label: 'Three' }
+]
 /* container components */
 class SamplePage extends React.Component {
     constructor(props, context){
       super(props, context);
 
       this.state = {
+          selectedOption: '',
           value: 85,
           items: [
               { id: 1, name: 'First Action' },
@@ -19,15 +28,18 @@ class SamplePage extends React.Component {
               { id: 4, name: 'Four Action' }
           ]
       }
-
-      this.handleSelectedValue = this.handleSelectedValue.bind(this);
+      this.handleChange = this.handleChange.bind(this);
     }
 
-    handleSelectedValue(item) {
-        console.log(item);
+    handleChange(selectedOption){
+        this.setState({ selectedOption });
+        console.log(`Selected: ${selectedOption.label}`);
+        console.log(selectedOption);
     }
-
+    
     render() {
+        const { selectedOption } = this.state;
+
         return (
             <div>
                 <h1>Sample Page</h1>
@@ -38,6 +50,15 @@ class SamplePage extends React.Component {
                 <div>
                     <h3>Demo Dropdown Multi Level</h3>
                     <NavMultiLevel />
+                </div>
+
+                <div>
+                    <h3>Demo React Select</h3>
+                    <Select name="form-field-name"
+                            value={selectedOption}
+                            onChange={this.handleChange}
+                            options={list}
+                    />
                 </div>
             </div>
         );
