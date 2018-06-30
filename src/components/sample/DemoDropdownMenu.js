ca1337af012745e3c31139e00c0f986803e969b9
anhvodhvn <anh.voduy@harveynash.vn>
2018-06-30 18:07:07 +0700

    clean up

--- src/components/sample/DemoDropdownMenu.js
+++ src/components/sample/DemoDropdownMenu.js
@@ -1,5 +1,4 @@
 import React, {PropTypes} from 'react';
-//import { Link } from 'react-router';
 import { DropdownButton, MenuItem } from 'react-bootstrap';
 
 const DemoDropdownMenu = ({items, handleSelectedValue}) => {
