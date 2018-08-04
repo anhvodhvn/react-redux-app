50e3c336a69d995587712d04dd8fa0c86d63468f
anhvodhvn <anh.voduy@harveynash.vn>
2018-08-04 23:30:34 +0700

    validate redux-form

---
+++ src/components/product/edit/validate.js
@@ -0,0 +1,51 @@
+const DEFAULT_MIN_LENGTH = 3;
+const DEFAULT_MAX_LENGTH = 50;
+
+const validation = values => {
+    let errors = {};
+    
+    // ['leaveType', 'notes', 'start_date', 'start_time', 'end_date', 'end_time'].forEach(field => {
+    //     if(!values[field]) errors[field]= 'Required';
+    // });
+
+    // ['start_date', 'end_date'].forEach((field) => {
+    //     if(values[field] && new Date(values[field]) <= (new Date()))
+    //         errors[field] = `Value must be larger current date`;
+    // });
+
+    // ['notes'].forEach((field) => {
+    //     if(values[field] && values[field].length < DEFAULT_MIN_LENGTH)
+    //         errors[field] = `Must be at least ${DEFAULT_MIN_LENGTH} characters`;
+    // });
+
+    // ['notes'].forEach((field) => {
+    //     if(values[field] && values[field].length > DEFAULT_MAX_LENGTH)
+    //         errors[field] = `Must be at least ${DEFAULT_MAX_LENGTH} characters`;
+    // });
+
+    // if(Array.isArray(values.leaves) && values.leaves.length){
+    //     let membersArrayErrors = [];
+    //     values.leaves.forEach((item, index) => {
+    //         let memberErrors = {};
+    //         if (!item || !item.leave_date) {
+    //             memberErrors.leave_date = 'Required';
+    //             membersArrayErrors[index] = memberErrors;
+    //         }
+    //         if (!item || !item.start_time) {
+    //             memberErrors.start_time = 'Required';
+    //             membersArrayErrors[index] = memberErrors;
+    //         }
+    //         if (!item || !item.end_time) {
+    //             memberErrors.end_time = 'Required';
+    //             membersArrayErrors[index] = memberErrors;
+    //         }
+    //     });
+    //     if (membersArrayErrors.length) {
+    //         errors.leaves = membersArrayErrors;
+    //     }
+    // }
+
+    return errors;
+};
+export default validation;
+
